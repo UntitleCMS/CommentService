982bc8c88675c93e2857af872d5f5db253cc6623
VillSource <anirut1800@gmail.com>
2023-09-29 20:04:29 +0700

    deploy: ☁️add docker support

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,9 +3,10 @@ import mongoose from "mongoose";
 const DB_HOST = "127.0.0.1";
 const DB_PORT = "27017";
 const DB_NAME = "comment";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
 const connectDatabase = async () => {
-  const connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+  const connectionString = CONNECTION_STRING;
   await mongoose
     .connect(connectionString)
     .then(() => {
